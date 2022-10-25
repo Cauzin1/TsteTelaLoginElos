6dcf6c0478d62707baab28d02ffbf612d2784968
Cauzin1 <76188228+Cauzin1@users.noreply.github.com>
2022-10-25 13:23:12 -0300

    Tela básica
    A ideia era navegar entre os botões, só que ainda não consegui implementar a navegação. Como estava dando muito erro, criei um repositório de teste só para relembrar como funciona a navegação por hiperlinks através do html/js/css e com isso poder fazer um protótipo para o site. A ideia primeiro é deixar funcional e depois "bonito".

---
+++ TesteLogin/index.js
@@ -0,0 +1,32 @@
+/*class CustomSelect {
+    constructor(OriginalSelect) {
+        this.originalSelect = OriginalSelect;
+        this.customSelect = document.createElement('div');
+        this.customSelect.classList.add('select');
+        this.originalSelect.querySelectorAll('option').forEach(optionElement => {
+            const itemElement = document.createElement('div');
+            itemElement.classList.add('select_item');
+            itemElement.textContent = optionElement.textContent;
+            this.customSelect.appendChild(itemElement);
+            itemElement.addEventListener('click', () => {
+                if(item)
+                        });
+        });
+        itemElement.originalSelect.insertAdjacentElement('afterend', this.customSelect);
+        
+    }
+    _select(itemElement){
+
+    }
+    _deselect(itemElement){
+    
+    }
+    
+    
+}
+
+
+
+document.querySelectorAll(".custom-select").forEach((select) => {
+    new CustomSelect(selectElement);
+});*/
